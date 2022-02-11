020cb4d2b376ed755f536c38cd1129a1356ed312
Wycctrujjla109121 <56939296+Wycctrujjla109121@users.noreply.github.com>
2022-02-11 03:11:12 +0300

    Add files via upload

--- js/popUpVisibility.js
+++ js/popUpVisibility.js
@@ -1,20 +1,28 @@
 // Появление popUp
 document.querySelector('.feedback__button').addEventListener('click', clickFeedback, true)
 
-function clickFeedback(){
-    if (document.querySelector('.eye').classList.contains('eye__display') != true)
-    {
-        document.querySelector('.eye').classList.add('eye__display')
+function clickFeedback(event) {
+    let name = document.querySelector('.feedback__input__name').value
+    let email = document.querySelector('.feedback__input__email').value
+    let checkbox = document.querySelector(".feedback__checkbox").checked
+
+    if (name != '' && email != '' && checkbox == true){
+        if (document.querySelector('.eye').classList.contains('eye__display') != true) {
+            document.querySelector('.eye').classList.add('eye__display')
+        }
+    }
+    else{
+        event.preventDefault()
     }
 }
 
 document.querySelector('.eye__quit').addEventListener('click', clickPopQuit, true)
 
-function clickPopQuit(){
-    if(document.querySelector('.eye').classList.contains("eye__display"))
-    {
+function clickPopQuit() {
+    if (document.querySelector('.eye').classList.contains("eye__display")) {
         document.querySelector('.eye').classList.remove('eye__display');
         document.querySelector('.feedback__input__name').value = ''
         document.querySelector('.feedback__input__email').value = ''
+        document.querySelector(".feedback__checkbox").checked = false
     }
 }
